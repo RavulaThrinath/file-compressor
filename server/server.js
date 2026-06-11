863a2460c590e585909bc8fff4ec5bc5d4b73f0d
Thrinath Ravula <thrinathravual@gmail.com>
2026-06-11 21:17:53 +0530

    Fix CORS origin

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(
 
 const upload = multer({ dest: "uploads/" });
 
-app.post("/https://file-compressor-api-1nlu.onrender.com/compress", upload.single("image"), async (req, res) => {
+app.post("/compress", upload.single("image"), async (req, res) => {
   try {
     const quality = Number(req.body.quality) || 60;
 
